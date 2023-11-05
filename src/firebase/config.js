49160667c28042eec0b3c07b29f10d6a12d0d349
hanif365 <m.a.hanifkhan64@gmail.com>
2023-11-05 12:29:20 +0600

    Fixed bug

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDM3etKVR28yv7uuhWX2KfJaBmWMXCR2EE",
-  authDomain: "myimagegallery1.firebaseapp.com",
-  projectId: "myimagegallery1",
-  storageBucket: "myimagegallery1.appspot.com",
-  messagingSenderId: "134151180045",
-  appId: "1:134151180045:web:afb40f02bb26e88787ea44",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
